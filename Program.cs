c279d8021989e94682289c16f4f6bc3d780ffd5d
mike_ <mike_@WINDOWSLAPTOP>
2022-12-18 17:29:55 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿// See https://aka.ms/new-console-template for more information
+//https://www.youtube.com/watch?v=l6s7AvZx5j8&t=1904s
+//using ConsoleUI.Models;
+using static System.Net.WebRequestMethods;
+
+using System;
+using System.Collections.Generic;
+//using ConsoleUI.WithoutGenerics;
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+
+            DemonstrateTextFileStorage();
+
+            Console.WriteLine("Hello, World!");
+            Console.Write("Press enter to shut down...");
+            Console.ReadLine();
+        }
+        private static void DemonstrateTextFileStorage()
+        {
+            List<Person> people = new();
+            List<LogEntry> logs = new();
+            PopulateLists(people, logs);
+
+
+
+            System.Console.WriteLine("\nNew way of doing things Generics");
+            string peopleFileGenerics = @"C:\Temp\peopleGenerics.csv";
+            string logFileGenerics = @"C:\Temp\logsGenerics.csv";
+            
+            GenericTextFileProcessor.SaveToTextFile<Person>(people, peopleFileGenerics);
+            var newPeopleGenerics = GenericTextFileProcessor.LoadFromTextFile<Person>(peopleFileGenerics);
+            foreach (var p in newPeopleGenerics)
+            {
+                Console.WriteLine($"{p.FirstName} {p.LastName} (IsAlive = {p.IsAlive})");
+            }
+
+            GenericTextFileProcessor.SaveToTextFile<LogEntry>(logs, logFileGenerics);
+            var newLogsGenerics = GenericTextFileProcessor.LoadFromTextFile<LogEntry>(logFileGenerics);
+            foreach (var log in newLogsGenerics)
+            {
+                Console.WriteLine($"{log.ErrorCode}: {log.Message} at {log.TimeOfEvent.ToShortTimeString()}");
+            }
+
+
+            System.Console.WriteLine("\nOld way of doing things - non - generics");
+            string peopleFile = @"C:\Temp\people.csv";
+            string logFile = @"C:\Temp\logs.csv";
+            
+            OriginalTextFileProcessor.SavePeople(people, peopleFile);
+            var newPeople = OriginalTextFileProcessor.LoadPeople(peopleFile);
+            foreach (var p in newPeople)
+            {
+                Console.WriteLine($"{p.FirstName}{p.LastName} (IsAlive + {p.IsAlive})");
+            }
+
+            OriginalTextFileProcessor.SaveLogs(logs, logFile);
+            var newLogs = OriginalTextFileProcessor.LoadLogs(logFile);
+            foreach (var log in newLogs)
+            {
+                Console.WriteLine($"{log.ErrorCode}{log.Message} (IsAlive + {log.TimeOfEvent.ToShortTimeString()})");
+            }
+
+
+
+        }
+        private static void PopulateLists(List<Person> people, List<LogEntry> logs)
+        {
+            people.Add(new Person { FirstName = "Steve", LastName = "Jones" });
+            people.Add(new Person { FirstName = "John", LastName = "Doe", IsAlive = false });
+            people.Add(new Person { FirstName = "Joe", LastName = "Frank" });
+
+            logs.Add(new LogEntry { Message = "Minor Error Don't worry", ErrorCode = 0035 });
+            logs.Add(new LogEntry { Message = "PSU on fire", ErrorCode = 29 });
+            logs.Add(new LogEntry { Message = "Hair on Fire", ErrorCode = 57 });
+        }
+
+    }
+}
+
---
+++ WithGenerics/GenericTextFileProcessor.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Runtime.CompilerServices;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+/**  Visual Studio Notes
+ * CTRL . Segestions
+ * F12 If on a Class.Something it will take you there.
+ * CTRL F12 Takes you to the implementation of the interface.
+ * 
+ * Options > Text Editor > Code Cleanup
+ * 
+ * 
+ * 
+ */
+
+namespace ConsoleUI 
+{
+    internal class GenericTextFileProcessor//  <S>  You can do it at class level.
+    {
+                                                                                 // Limitations/ constraint
+                                                                                 // Must be class
+        // 2 Must have an empty constructor new().  Not required, but helps you dial in what type of object is put in T0
+        //public static List<T, U> LoadFromTextFile<T> (string filePath) where T : class, new() where U : class
+        public static List<T> LoadFromTextFile<T> (string filePath) where T : class, new()
+        {
+            var lines = System.IO.File.ReadAllLines(filePath).ToList();
+            List<T> output = new List<T>();
+            T entry = new T();  //Limit new() is here.  If limit is not used, this line will error out
+            var cols = entry.GetType().GetProperties();  //Reflection used here.  Expensive.  Doing at file write so its ok.
+            //Checks to be sure we have at least one header row and one data row
+            if (lines.Count < 2)
+            {
+                throw new IndexOutOfRangeException("The file was either empty or missing.");
+            }
+            
+            // Splits the header into one column header per entry
+            var headers = lines[0].Split(',');
+
+            // Removes the header row from the lines so we don't
+            // have to worry about skipping over that first row.
+            lines .RemoveAt(0);
+
+            foreach (var row in lines)
+            {
+                entry = new T();
+
+                // Splits the row into individual columns. Now the index
+                // of this row matches the index of the header so the
+                // FirstName column header lines up with the FirstName
+                // value in this row.
+                var vals = row.Split(',');
+
+                // Loops through each header entry so we can compare that
+                // against the list of columns from reflection. Once we get
+                // the matching column, we can do the "SetValue" method to
+                // set the column value for our entry variable to the vals
+                // item at the same index as this particular header.
+                for (var i = 0; i < headers.Length; i++)
+                {
+                    foreach (var col in cols)
+                    {
+                        
+                        if (col.Name == headers[i])
+                        {
+                            col.SetValue(entry, Convert.ChangeType(vals[i], col.PropertyType));
+                        }
+                    }
+                }
+                output.Add(entry);
+            }
+            return output;
+        }
+
+        public static void SaveToTextFile<T>(List<T> data, string filePath) where T : class//, new() I guess we did not need this limit
+        {
+            List<string> lines = new List<string>();
+            StringBuilder line = new StringBuilder();
+
+            if (data == null || data.Count == 0)
+            {
+                throw new ArgumentNullException("data", "You must populate the data parameter with at least o");
+                
+            }
+            var cols = data[0].GetType().GetProperties();
+            // Loops through each column and gets the name so it can comma
+            // separate it into the header row.
+            foreach (var col in cols)
+            {
+                line.Append(col.Name);
+                line.Append(",");
+            }
+
+
+
+            // Adds the column header entries to the first line (removing
+            // the last comma from the end first).
+            lines.Add(line.ToString().Substring(0, line.Length - 1));
+
+            foreach (var row in data)
+            {
+                line = new StringBuilder();
+
+                foreach (var col in cols)
+                {
+                    line.Append(col.GetValue(row));
+                    line.Append(",");
+                }
+
+                // Adds the row to the set of lines (removing
+                // the last comma from the end first).
+                lines.Add(line.ToString().Substring(0, line.Length - 1));
+            }
+
+            System.IO.File.WriteAllLines(filePath, lines);
+        }
+    }
+}
+
+
